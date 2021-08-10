b63e536d1930e4b4f21614d521cbf084cb14c1d7
madbomb122 <madbomb122@gmail.com>
2021-08-10 09:51:32 -0700

    Update MKV-NoSubtitleList.ps1

--- MKV-NoSubtitleList.ps1
+++ MKV-NoSubtitleList.ps1
@@ -1,5 +1,5 @@
 #change This to path of the folder(s) you want searched
-$SearchPath = 'M:\Movies'
+$SearchPath = 'M:'
 
 #In the function change the path of mkvinfo
 $MkvToolPath = 'D:\mkvtoolnix'
@@ -32,11 +32,34 @@ $Pathlen = $SearchPath.Length
 If($SearchPath.Substring($Pathlen -1,1) -eq '\'){ $SearchPath = $SearchPath.Substring(0,$Pathlen -1) }
 $Pathlen = $MkvToolPath.Length
 If($MkvToolPath.Substring($Pathlen -1,1) -eq '\'){ $MkvToolPath = $MkvToolPath.Substring(0,$Pathlen -1) }
+$Subdir1 = ''
+$MissingSubArr = @()
 
+Write-Host "Getting list of all MKVs in " -ForegroundColor 'Red' -BackgroundColor 'Black' -NoNewLine
+Write-Host $SearchPath -ForegroundColor 'Green' -BackgroundColor 'Black'
+		
 $List = @(Get-ChildItem -Path "$SearchPath\*.mkv" -Recurse | Select-Object -ExpandProperty FullName)
-Foreach($File in $List) {
-	$tmp = Get-MKVTrackInfo $File
-	If('subtitles' -NotIn $tmp.tracktype){ $File }
+Foreach($FilePath in $List) {
+	$Subdir2 = Split-Path $FilePath
+	If($Subdir1 -ne $Subdir2) {
+		Write-Host "Checking MKVs in: " -ForegroundColor 'Red' -BackgroundColor 'Black' -NoNewLine
+		Write-Host $Subdir2 -ForegroundColor 'Green' -BackgroundColor 'Black'
+		$Subdir1 = $Subdir2
+	}
+	$tmp = Get-MKVTrackInfo $FilePath
+	If('subtitles' -NotIn $tmp.tracktype){ $MissingSubArr += $FilePath }
 }
 
-Read-Host 'Done'
+Write-Host
+
+If($MissingSubArr.Count -ne 0) {
+	Write-Host "Files with no subtitles:" -ForegroundColor 'Red' -BackgroundColor 'Black' 
+	$MissingSubArr
+} Else {
+	Write-Host "All MKVs in " -ForegroundColor 'Red' -BackgroundColor 'Black' -NoNewLine
+	Write-Host $SearchPath -ForegroundColor 'Green' -BackgroundColor 'Black' -NoNewLine
+	Write-Host " have subtitles" -ForegroundColor 'Red' -BackgroundColor 'Black'
+}
+
+Write-Host
+Read-Host "Done, press 'Enter' to close"
